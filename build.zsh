d937035704e7aa74f28ea023f7eb1250d1911ad7
ochmanskide <github@ochmanski.de>
2022-10-25 22:00:42 +0200

    add gradle tasks

--- build.zsh
+++ build.zsh
@@ -10,17 +10,17 @@
 #docker login
 cd $GITHUB/base/images/debian/11-bullseye/mandrel/22-2/java/17/mandrel-22-2-gradle-7-5-java-17 || exit
 DOCKER_HUB_HOST=ochmanskide
-#GROUP_ID=$(gradle rootProjectGroupRaw -q)
-#ARTIFACT_ID=$(gradle rootProjectNameRaw -q)
-#IMAGE_TAG=$(gradle rootProjectVersionRaw -q)
+GROUP_ID=$(gradle rootProjectGroupRaw -q)
+ARTIFACT_ID=$(gradle rootProjectNameRaw -q)
+IMAGE_TAG=$(gradle rootProjectVersionRaw -q)
 
-GROUP_ID='base.images.debian.11-bullseye.mandrel.22-2.java.17'
-ARTIFACT_ID='mandrel-22-2-gradle-7-5-java-17'
-IMAGE_TAG=$(gradle printVersion -q)
+#GROUP_ID='base.images.debian.11-bullseye.mandrel.22-2.java.17'
+#ARTIFACT_ID='mandrel-22-2-gradle-7-5-java-17'
+#IMAGE_TAG=$(gradle printVersion -q)
 
-docker build -t "$DOCKER_HUB_HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG-slim . # --squash
-docker run --rm -ti --privileged --entrypoint /bin/bash "$DOCKER_HUB_HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG-slim
-docker image rm "$DOCKER_HUB_HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG-slim
+docker build -t "$DOCKER_HUB_HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG .
+docker run --rm -ti --privileged --entrypoint /bin/bash "$DOCKER_HUB_HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG
+docker image rm "$DOCKER_HUB_HOST"/"$GROUP_ID"/"$ARTIFACT_ID":$IMAGE_TAG
 echo
 
 if [[ "$IMAGE_TAG" == *-SNAPSHOT ]]
